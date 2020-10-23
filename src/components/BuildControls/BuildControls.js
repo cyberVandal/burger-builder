3537425d6490d54469b52330264c5f3621ce4bce
cyberVandal <goran_mitkovski@yahoo.com>
2020-10-23 12:09:28 +0200

    Add and Remove ingredients dynamically

---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import classes from './BuildControls.module.css';
+import BuildControl from '../BuildControls/BuildControl/BuildControl';
+
+const controls = [
+
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+
+];
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        <p>Current Price: <stron>{props.price.toFixed(2)}</stron> </p>
+        {controls.map(ctrl => (
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientsAdded(ctrl.type)}
+                removed={() => props.ingredientsRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
+
+            />
+        ))}
+    </div>
+);
+
+export default buildControls;
